e62a5e2b12c2f6d7787078224684f2c3892c5bab
p-a-s-c-a-l <pascal@cismet.de>
2016-11-09 10:44:09 +0100

    #27 start scripts updated

--- src/udm2020-diDist/install.bat
+++ src/udm2020-diDist/install.bat
@@ -1,7 +1,7 @@
 @ECHO OFF
 
 chdir /d %~dp0
-call %~dp0bin\setenv.cmd
+call %~dp0bin\setenv.cmd > install-cids.log
 
 chdir /d %~dp0
 call %~dp0gen\gen_cids\install.cmd > install-cids.log
--- src/udm2020-diDist/run.bat
+++ src/udm2020-diDist/run.bat
@@ -4,10 +4,10 @@ chdir /d %~dp0
 call %~dp0bin\setenv.cmd
 
 chdir /d %~dp0
-start server\udm2020-di\console.cmd
+start server\udm2020-di\server.cmd
 
 chdir /d %~dp0
-start server\udm2020-di-rest\console.cmd
+start server\udm2020-di-rest\server-rest.cmd
 
 chdir /d %~dp0client\uim2020-html5-demonstrator-master\
 start http-server ./app -a 0.0.0.0 -p 80 --cors -c-1
