e3ffc2d4be2164c2c3c964ecf7c835cba60b53c2
p-a-s-c-a-l <pascal@cismet.de>
2017-04-12 13:33:23 +0200

    #28 update udm2020-di Distribution

--- src/udm2020-diDist/install.bat
+++ src/udm2020-diDist/install.bat
@@ -7,7 +7,4 @@ chdir /d %~dp0
 call %~dp0gen\gen_cids\install.cmd > install-cids.log
 
 chdir /d %~dp0
-call %~dp0gen\gen_rest\install.cmd > install-rest.log
-
-chdir /d %~dp0
 call %~dp0gen\gen_html5\install.cmd > install-html5.log
